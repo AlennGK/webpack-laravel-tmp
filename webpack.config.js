58e425a1676b9d91d4650a55c9091ed36cb296a4
AlennGK <AlennGK@gmail.com>
2017-02-12 16:47:15 +0100

    Separation
    - Separated dev and prod mod.
    - webpack.config optimalization

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 var path = require('path');
 var htmlWebPack = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack')
+var ProvidePlugin = require('webpack/lib/ProvidePlugin');
+
 
 module.exports = {
 	devtool: 'cheap-eval-source-map',
@@ -11,36 +14,74 @@ module.exports = {
 	},
 
 	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: "[name].bundle.js"
+		publicPath: 'http://localhost:8080',
+		path: path.join(__dirname, 'public'),
+		filename: "js/[name].bundle.js"
 	},
 
 	module: {
 		loaders: [
+		// JS
 			{
 				test: /\.js$/,		// ON WHAT TYPE USE THIS LOADER
 				loader: 'babel',
 				include: path.join(__dirname, 'src', 'js'),
 			},
-
+		// STYLE
 		 	{
-		 		test: /\.sass$/,
+		 		test: /\.(sass|scss|css)$/,
 		 		loader: ExtractTextPlugin.
 		 			extract({
 		 			fallback: 	'style-loader',
 		 			use:		'css-loader!sass-loader'
 		 		}),
 		 	},
+		// FILES
+			{
+				test: /\.(jpe?g|png|gif|svg|ico)$/i,
+				loader: "file-loader?name=[path][name].[ext]&context=./src/assets"
+			},
+		// FONTS
+			{
+				test: /\.(otf|eot|svg|ttf|woff)$/,
+				loader: 'url-loader?limit=10000'
+			},
+
+			{
+				test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "url-loader?limit=10000"
+			},
+
+			{
+				test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+				loader: 'file-loader'
+			},
+		// BOOTSTRAP
+			{
+				test: /bootstrap\/public\/js\/umd\//,
+				loader: 'imports?jQuery=jquery'
+			},
 		 ],
 	},
 
 	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
+		contentBase: path.join(__dirname, 'src', 'assets'),
 		inline: true,
 		stats: {
+			errors: true,
 			colors: true,
-			reasons: true,
-			chunks: false
+			errorDetails: true,
+			reasons: false,
+			hash: false,
+			version: false,
+			timings: false,
+			assets: false,
+			chunks: false,
+			modules: false,
+			children: false,
+			source: false,
+			warnings: false,
+			publicPath: false
 		}
 	},
 	plugins: [
@@ -57,6 +98,13 @@ module.exports = {
 			chunks: ['test']
 		}),
 
-		new ExtractTextPlugin('style.css')
-	]
+		new ExtractTextPlugin('./css/[name].style.css'),
+
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+			"window.jQuery": "jquery",
+			"Tether": 'tether' // Bootstrap v4 problem
+		})
+	],
 };
